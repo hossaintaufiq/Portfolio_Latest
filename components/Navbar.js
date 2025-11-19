f87f8f092d73e14b9851ceca2098f3d48c3398d3
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2025-11-19 07:26:49 +0600

    navbar updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -84,16 +84,13 @@ export default function Navbar() {
               <Link
                 key={link.href}
                 href={link.href}
-                className={`relative rounded-full px-4 py-2 transition-all duration-300 lg:px-5 ${
+                className={`rounded-full px-4 py-2 transition-all duration-300 lg:px-5 ${
                   active
                     ? "text-white bg-gradient-to-r from-cyan-500/20 to-blue-500/20 border border-cyan-500/30"
                     : "text-white/70 hover:text-white hover:bg-white/10"
                 }`}
               >
                 {link.label}
-                {active && (
-                  <span className="absolute -bottom-1 left-1/2 h-0.5 w-2/3 -translate-x-1/2 rounded-full bg-gradient-to-r from-cyan-400 to-blue-400" />
-                )}
               </Link>
             );
           })}
